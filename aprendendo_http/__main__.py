00ad3d8b355bb16632430dac06fcf0c3bcc90549
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-04-09 23:33:09 -0300

    Beautify message :lipstick:

--- aprendendo_http/__main__.py
+++ aprendendo_http/__main__.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 @app.route('/receive', methods=['POST', 'GET', 'PUT', 'DELETE', 'PATCH'])
 def receive():
     message = (
-        f'{request.headers}\n\n'
-        f'{request.method} {request.url}\n\n'
-        f'{request.data}\n\n'
+        '-----------------\n'
+        f'Headers: \n{request.headers}\n\n'
+        f'Method: \n{request.method}\n\n'
+        f'URL: \n{request.url}\n\n'
+        f'Body: \n{request.data}\n'
+        '-----------------'
     )
     print(message)
     return message
